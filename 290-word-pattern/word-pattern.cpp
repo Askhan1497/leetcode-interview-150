b08bc64aee07eab85ff4bf15e5dc1d5949ac993d
Aasif Khan <83283933+akay41024@users.noreply.github.com>
2023-12-01 23:16:24 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (70.53%) - LeetSync

---
+++ 290-word-pattern/word-pattern.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, string> m;
+        vector<string> word;
+        int i =0;
+        string temp ="";
+        while(i<s.size()){
+            if(s[i] == ' ')
+            {
+                word.push_back(temp);
+                temp = "";
+            }
+            else{
+                temp += s[i];
+            }
+            i++;
+        }
+        word.push_back(temp);
+        if(word.size() != pattern.size())
+        {
+            return false;
+        }
+        set<string> sp;
+        for(int i =0; i <pattern.size(); i++)
+        {
+            if(m.find(pattern[i]) != m.end())
+            {   
+                if(m[pattern[i]] != word[i])
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if(sp.count(word[i]) > 0)
+                {
+                    return false;
+                }
+                sp.insert(word[i]);
+                m[pattern[i]] = word[i];
+            }
+        }
+        return true;
+
+    }
+};
